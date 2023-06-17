14a757ba6e161ccf262fc8008ece1007d891b3b0
mays4 <maysalani84@yahoo.com>
2023-06-17 02:25:37 -0400

    Added origin to cors

--- server/index.js
+++ server/index.js
@@ -30,7 +30,9 @@ app.use(helmet.crossOriginResourcePolicy({plicy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
-app.use(cors());
+app.use(cors({
+  origin:['https://beconnected-znrm.onrender.com']
+}));
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 
 /* File Storage */
