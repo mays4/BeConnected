556b0cb69906b6706b1c7182f196f43d04f016c2
mays4 <maysalani84@yahoo.com>
2023-06-19 15:41:25 -0400

    Changed in profile page

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box,useMediaQuery,useTheme} from '@mui/material';
+import { Box,Typography,useMediaQuery,useTheme} from '@mui/material';
 import { useEffect,useState } from 'react';
 import {useParams} from "react-router-dom"
 import { useSelector } from 'react-redux';
@@ -8,7 +8,7 @@ import UserCard from 'pages/Features/UserCard';
 import FriendListCard from 'pages/Features/FriendListCard';
 import PostsCard from 'pages/Features/PostsCard';
 import MyPost from 'pages/Features/MyPost';
-
+import FlexBox from "components/FlexBox";
 const Profile= ()=> {
   const [user,setUser]=useState(null);
   const {userId}=useParams();
@@ -34,8 +34,10 @@ const Profile= ()=> {
   return (
 <Box >
       <Navbar/>
+     
       <Box width="100%" padding="2rem 6%" display={isNonMobileScreens?"flex":"block"} gap="2rem"justifyContent="center">
       <Box flexBasis={isNonMobileScreens?"25%":undefined}  sx={{marginTop:"1.80rem"}} >
+        
 <UserCard userId={userId} picturePath={user.picturePath} backgroundColor="red"/>
 
 <Box margin="1rem 0" />
@@ -43,14 +45,28 @@ const Profile= ()=> {
   <FriendListCard userId={userId} />
 )}
 </Box>
+
      {isUser?(
+    
+    
+      
+  
       <Box  flexBasis={isNonMobileScreens?"70%":undefined} mt={isNonMobileScreens? undefined:"2rem"} sx={{marginTop:"1.80rem"}}
       >
+        
+         <FlexBox sx={{width:"100%",
+            backgroundColor:palette.primary.dark,
+            alignItems:"center",
+            borderRadius:"0.5rem",
+            padding:"0.25rem 1rem",
+            marginBottom:"0.50rem"}}>  <Typography>This is Your profile personal space</Typography></FlexBox>
             <MyPost picturePath={user.picturePath} /> 
-          
+               
    <Box m="1rem"/>
+  
    <PostsCard  userId={userId} isProfile/>
-      </Box>
+     </Box>
+      
      ):(<Box flexBasis={isNonMobileScreens?"70%":undefined} mt={isNonMobileScreens? undefined:"2rem"} 
      >
       
